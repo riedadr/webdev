77c9a834684095eb3a0ebd76a7ffa3a313801bf6
karsten <riedadr@gmail.com>
2022-05-17 09:32:15 +0200

    vl js 3

---
+++ objekte.js
@@ -0,0 +1,55 @@
+document.write("<div class='block'>");
+
+console.log("=== Objekte ===");
+
+
+
+
+
+var obj = {a: 10, b: "Hallo Welt!" , f: function () {
+    console.log("you called me!");
+}};
+
+console.log(obj.b);
+
+delete obj.b;
+console.log(obj);
+
+obj.f()
+
+
+function area(param) {
+    if (param == undefined) {
+        throw Error("Param undefined!")
+    }
+    return param.width * param.height;
+}
+let a = area({width: 100, height: 13})
+document.write("100 * 13 = " + a);
+try {
+    document.write("100 * 13 = " + area());
+} catch (error) {
+    console.log(error.message);
+}
+
+
+let person = {name: "Peter", age: 69}
+
+let person2 = Object.create(person);
+console.log(person.name, person2.name); //Peter Peter
+
+person2["name"] = "Dave";
+person2["neu"] = "Ich bin neu!";
+
+console.log(person2);
+
+Object.freeze(person2);
+person2.age = 22; //wird nicht mehr geändert
+
+console.log(person2);
+
+
+
+
+
+document.write("</div>");
